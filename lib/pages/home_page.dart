7f31d7b73ace5478809b2fabed383d692af4066e
arnav-nirankari <arnav.nirankari@student.ufv.ca>
2022-11-01 22:42:05 +0530

    Day 11 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -28,11 +28,11 @@ class _HomePageState extends State<HomePage> {
   }
 
   loadData() async {
-    await Future.delayed(const Duration(seconds: 2));
+    await Future.delayed(const Duration(seconds: 0));
     final catalogJson =
         await rootBundle.loadString("assets/files/catalog.json");
     final jsonDecoded = await jsonDecode(catalogJson);
-    final prodData = jsonDecoded["products"];
+    final prodData = await jsonDecoded["products"];
     CatalogModel.items =
         List.from(prodData).map<Item>((item) => Item.fromMap(item)).toList();
     setState(() {});
@@ -47,15 +47,55 @@ class _HomePageState extends State<HomePage> {
           "Jumbo Electronics",
         ),
       ),
-      body: ListView.builder(
-        itemCount: CatalogModel.items.length,
-        physics: const ScrollPhysics(),
-        itemBuilder: (BuildContext context, int index) {
-          return ItemWidget(
-            item: CatalogModel.items[index],
+      body: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 16,
+        ),
+        itemBuilder: ((context, index) {
+          final item = CatalogModel.items[index];
+          return Card(
+            clipBehavior: Clip.antiAlias,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            child: GridTile(
+                header: Container(
+                    padding: EdgeInsets.all(12),
+                    decoration: const BoxDecoration(
+                      color: Colors.deepPurple,
+                    ),
+                    child: Text(
+                      item.name,
+                      style: TextStyle(color: Colors.white),
+                    )),
+                footer: Container(
+                    padding: EdgeInsets.all(12),
+                    decoration: const BoxDecoration(
+                      color: Colors.black45,
+                    ),
+                    child: Text(
+                      item.price.toString(),
+                      style: TextStyle(color: Colors.white),
+                    )),
+                child: Image.network(
+                  item.image,
+                  fit: BoxFit.contain,
+                )),
           );
-        },
+        }),
+        itemCount: CatalogModel.items.length,
       ),
+
+      // ListView.builder(
+      //   itemCount: CatalogModel.items.length,
+      //   physics: const ScrollPhysics(),
+      //   itemBuilder: (BuildContext context, int index) {
+      //     return ItemWidget(
+      //       item: CatalogModel.items[index],
+      //     );
+      //   },
+      // ),
       // : const Center(child: CircularProgressIndicator()), // Center
       drawer: MyDrawer(),
     ); // Material;
